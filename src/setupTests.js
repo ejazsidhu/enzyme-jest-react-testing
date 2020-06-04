fbe7b249b9fa240e8cce0d0efc59fd2dc13b7ed2
ejazsidhu <ejazsidhu@gmail.com>
2020-06-04 06:01:16 +0500

    inistal enzyme and jest seting done

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,14 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom/extend-expect';
+// import '@testing-library/jest-dom/extend-expect';  this was default testing library option for this project
+
+// following are custom import for testing app with help of enzyme and jest
+
+import Enzyme from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({
+    adapter: new EnzymeAdapter(),
+    disableLifecycleMethods:true;
+});
